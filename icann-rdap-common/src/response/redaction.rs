95bb65ce5c066357fbba800c834048ab7fc10363
Adam Guyot <adam@cobenian.com>
2024-01-04 12:05:02 -0500

    more structure and scaffolding

--- icann-rdap-common/src/response/redaction.rs
+++ icann-rdap-common/src/response/redaction.rs
@@ -13,7 +13,11 @@ pub struct Name {
 
 #[derive(Serialize, Deserialize, Builder, Clone, Debug, PartialEq, Eq)]
 pub struct Reason {
-    description: String,
+  #[serde(rename = "description")]
+  pub description: Option<String>,
+
+  #[serde(rename = "type")]
+  pub type_field: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
@@ -32,7 +36,7 @@ pub struct Redaction {
   pub name: Name,
 
   #[serde[rename = "reason"]]
-  pub reason: Reason,
+  pub reason: Option<Reason>,
 
   #[serde[rename = "prePath"]]
   pub pre_path: Option<String>,
@@ -51,3 +55,52 @@ pub struct Redaction {
   
 
 }
+
+impl Default for Name {
+  fn default() -> Self {
+      Self {
+          // provide default values for the fields of Name
+      }
+  }
+}
+
+impl Default for Reason {
+  fn default() -> Self {
+      Self {
+          // provide default values for the fields of Reason
+      }
+  }
+}
+
+impl Default for Method {
+  fn default() -> Self {
+      Self::Removal // or whatever default you want
+  }
+}
+
+impl Redaction {
+  pub fn new() -> Self {
+      Self {
+          name: Name::default(),
+          reason: Reason::default(),
+          pre_path: None,
+          post_path: None,
+          path_lang: None,
+          replacement_path: None,
+          method: Method::default(),
+      }
+  }
+}
+
+// No, don't actually do this. Magic Fairyland right now. Tests to come soon
+// fn main() {
+// let redaction = Redaction::new()
+//     .name( Name { description: "foo".to_string()}) // replace with actual value
+//     .reason(Reason::default()) // replace with actual value
+//     .pre_path(Some("$.handle".to_string())) // just fake values for now
+//     .post_path(Some("$.entities[?(@.roles[0]=='registrant".to_string())) // we wouldn't have both pre_path and post_path, but I'm just showing both here
+//     .path_lang(Some("jsonpath".to_string())) 
+//     .replacement_path(Some("$.entities[?(@.roles[0]=='registrant')].vcardArray[1][?(@[0]=='contact-uri')]".to_string())) // again 
+//     .method(Method::Removal); // let's get the default value
+// }
+
