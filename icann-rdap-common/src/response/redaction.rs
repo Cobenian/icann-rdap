ad185902f4f308bb90f613db33394ec55cbe6ccc
Adam Guyot <adam@cobenian.com>
2024-01-04 14:20:21 -0500

    fixing formatting issues

--- icann-rdap-common/src/response/redaction.rs
+++ icann-rdap-common/src/response/redaction.rs
@@ -1,7 +1,6 @@
-// These are my first initial thoughts on how we _might_ structure a redacted member according to 
+// These are my first initial thoughts on how we _might_ structure a redacted member according to
 // draft-ietf-regext-rdap-redacted-16
 // I'm not saying this is correct, but let's get something down to get things started.
-
 
 use serde::{Deserialize, Serialize};
 
@@ -18,11 +17,11 @@ pub struct Name {
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 pub struct Reason {
-  #[serde(rename = "description")]
-  pub description: Option<String>,
+    #[serde(rename = "description")]
+    pub description: Option<String>,
 
-  #[serde(rename = "type")]
-  pub type_field: Option<String>,
+    #[serde(rename = "type")]
+    pub type_field: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
@@ -36,68 +35,65 @@ pub enum Method {
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 pub struct Redaction {
+    #[serde[rename = "name"]]
+    pub name: Name,
 
-  #[serde[rename = "name"]]
-  pub name: Name,
+    #[serde[rename = "reason"]]
+    pub reason: Option<Reason>,
 
-  #[serde[rename = "reason"]]
-  pub reason: Option<Reason>,
+    #[serde[rename = "prePath"]]
+    pub pre_path: Option<String>,
 
-  #[serde[rename = "prePath"]]
-  pub pre_path: Option<String>,
-  
-  #[serde[rename = "postPath"]]
-  pub post_path: Option<String>,
-  
-  #[serde[rename = "pathLang"]]
-  pub path_lang: Option<String>,
+    #[serde[rename = "postPath"]]
+    pub post_path: Option<String>,
 
-  #[serde[rename = "replacementPath"]]
-  pub replacement_path: Option<String>,
+    #[serde[rename = "pathLang"]]
+    pub path_lang: Option<String>,
 
-  #[serde(rename = "method")]
-  pub method: Method,
-  
+    #[serde[rename = "replacementPath"]]
+    pub replacement_path: Option<String>,
 
+    #[serde(rename = "method")]
+    pub method: Method,
 }
 
 impl Default for Name {
-  fn default() -> Self {
-      Self {
-          description: String::default(),
-          // provide default values for the fields of Name
-      }
-  }
+    fn default() -> Self {
+        Self {
+            description: String::default(),
+            // provide default values for the fields of Name
+        }
+    }
 }
 
 impl Default for Reason {
-  fn default() -> Self {
-      Self {
-          description: None,
-          type_field: None,
-          // provide default values for the fields of Reason
-      }
-  }
+    fn default() -> Self {
+        Self {
+            description: None,
+            type_field: None,
+            // provide default values for the fields of Reason
+        }
+    }
 }
 
 impl Default for Method {
-  fn default() -> Self {
-      Self::Removal // or whatever default you want
-  }
+    fn default() -> Self {
+        Self::Removal // or whatever default you want
+    }
 }
 
 impl Redaction {
-  pub fn new() -> Self {
-      Self {
-          name: Name::default(),
-          reason: Some(Reason::default()),
-          pre_path: None,
-          post_path: None,
-          path_lang: None,
-          replacement_path: None,
-          method: Method::default(),
-      }
-  }
+    pub fn new() -> Self {
+        Self {
+            name: Name::default(),
+            reason: Some(Reason::default()),
+            pre_path: None,
+            post_path: None,
+            path_lang: None,
+            replacement_path: None,
+            method: Method::default(),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -110,7 +106,9 @@ mod tests {
         //todo: acutally do this deserialization test, not just test the language
         // GIVEN
         let mut name = Redaction::new();
-        name.name = Name { description: "foo".to_string() };
+        name.name = Name {
+            description: "foo".to_string(),
+        };
 
         // WHEN
         let mut redaction = name;
@@ -118,16 +116,27 @@ mod tests {
         redaction.pre_path = Some("$.handle".to_string());
         redaction.post_path = Some("$.entities[?(@.roles[0]=='registrant".to_string());
         redaction.path_lang = Some("jsonpath".to_string());
-        redaction.replacement_path = Some("$.entities[?(@.roles[0]=='registrant')].vcardArray[1][?(@[0]=='contact-uri')]".to_string());
+        redaction.replacement_path = Some(
+            "$.entities[?(@.roles[0]=='registrant')].vcardArray[1][?(@[0]=='contact-uri')]"
+                .to_string(),
+        );
         redaction.method = Method::Removal;
 
         // THEN
         assert_eq!(redaction.name.description, "foo");
         assert_eq!(redaction.pre_path, Some("$.handle".to_string()));
-        assert_eq!(redaction.post_path, Some("$.entities[?(@.roles[0]=='registrant".to_string()));
+        assert_eq!(
+            redaction.post_path,
+            Some("$.entities[?(@.roles[0]=='registrant".to_string())
+        );
         assert_eq!(redaction.path_lang, Some("jsonpath".to_string()));
-        assert_eq!(redaction.replacement_path, Some("$.entities[?(@.roles[0]=='registrant')].vcardArray[1][?(@[0]=='contact-uri')]".to_string()));
+        assert_eq!(
+            redaction.replacement_path,
+            Some(
+                "$.entities[?(@.roles[0]=='registrant')].vcardArray[1][?(@[0]=='contact-uri')]"
+                    .to_string()
+            )
+        );
         assert_eq!(redaction.method, Method::Removal);
     }
 }
-
