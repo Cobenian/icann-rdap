6f4aebe9dbbb4584b3a0828a820f07cf3aa592a5
Adam Guyot <adam@cobenian.com>
2024-02-20 13:50:07 -0500

    Save point. This is not fully corret but we are very close

--- icann-rdap-cli/src/query.rs
+++ icann-rdap-cli/src/query.rs
@@ -238,37 +238,72 @@ fn do_output<'a, W: std::io::Write>(
         parent_type: response.rdap.get_type(),
     });
 
+    // This displays them all in multiple tables
+    // if let Some(redactions) = response.rdap.get_redaction_if_exists() {
+    //     for redaction in redactions.iter() {
+    //         // Assuming `redacted` is an instance of `Redacted`
+    //         // println!("!!!! -->  {:?}", redaction);
+    //         let mut skin = MadSkin::default_dark();
+    //         skin.set_headers_fg(Yellow);
+    //         skin.headers[1].align = Alignment::Center;
+    //         skin.headers[2].align = Alignment::Center;
+    //         skin.headers[3].align = Alignment::Center;
+    //         skin.headers[4].compound_style.set_fg(DarkGreen);
+    //         skin.headers[5].compound_style.set_fg(Magenta);
+    //         skin.headers[6].compound_style.set_fg(Cyan);
+    //         skin.headers[7].compound_style.set_fg(Red);
+    //         skin.bold.set_fg(DarkBlue);
+    //         skin.italic.set_fg(Red);
+    //         skin.quote_mark.set_fg(DarkBlue);
+    //         skin.table.set_fg(DarkGreen);
+    //         skin.table.align = Alignment::Center;
+    //         skin.inline_code.set_fgbg(Cyan, Reset);
+    //         skin.write_text_on(
+    //             write,
+    //             &redaction.to_md(MdParams {
+    //                 heading_level: 1,
+    //                 root: &response.rdap,
+    //                 parent_type: redaction.get_type(),
+    //                 check_types: &processing_params.check_types,
+    //                 options: &MdOptions::default(),
+    //                 req_data,
+    //             }),
+    //         )?;
+    //     }
+    // } else {
+    //     // do nothing for now
+    //     // Handle the case where there is no Redaction
+    //     // println!("XXXX No redaction found");
+    // }
+
     if let Some(redactions) = response.rdap.get_redaction_if_exists() {
-        for redaction in redactions.iter() {
-            // Assuming `redacted` is an instance of `Redacted`
-            // println!("!!!! -->  {:?}", redaction);
-            let mut skin = MadSkin::default_dark();
-            skin.set_headers_fg(Yellow);
-            skin.headers[1].align = Alignment::Center;
-            skin.headers[2].align = Alignment::Center;
-            skin.headers[3].align = Alignment::Center;
-            skin.headers[4].compound_style.set_fg(DarkGreen);
-            skin.headers[5].compound_style.set_fg(Magenta);
-            skin.headers[6].compound_style.set_fg(Cyan);
-            skin.headers[7].compound_style.set_fg(Red);
-            skin.bold.set_fg(DarkBlue);
-            skin.italic.set_fg(Red);
-            skin.quote_mark.set_fg(DarkBlue);
-            skin.table.set_fg(DarkGreen);
-            skin.table.align = Alignment::Center;
-            skin.inline_code.set_fgbg(Cyan, Reset);
-            skin.write_text_on(
-                write,
-                &redaction.to_md(MdParams {
-                    heading_level: 1,
-                    root: &response.rdap,
-                    parent_type: redaction.get_type(),
-                    check_types: &processing_params.check_types,
-                    options: &MdOptions::default(),
-                    req_data,
-                }),
-            )?;
-        }
+        let mut skin = MadSkin::default_dark();
+        skin.set_headers_fg(Yellow);
+        skin.headers[1].align = Alignment::Center;
+        skin.headers[2].align = Alignment::Center;
+        skin.headers[3].align = Alignment::Center;
+        skin.headers[4].compound_style.set_fg(DarkGreen);
+        skin.headers[5].compound_style.set_fg(Magenta);
+        skin.headers[6].compound_style.set_fg(Cyan);
+        skin.headers[7].compound_style.set_fg(Red);
+        skin.bold.set_fg(DarkBlue);
+        skin.italic.set_fg(Red);
+        skin.quote_mark.set_fg(DarkBlue);
+        skin.table.set_fg(DarkGreen);
+        skin.table.align = Alignment::Center;
+        skin.inline_code.set_fgbg(Cyan, Reset);
+
+        let all_redactions_md = redactions.as_slice().to_md(MdParams {
+            heading_level: 1,
+            root: &response.rdap,
+            parent_type: redactions[0].get_type(), // Assuming all redactions have the same type
+            check_types: &processing_params.check_types,
+            options: &MdOptions::default(),
+            req_data,
+        });
+
+        // Now `all_redactions_md` contains a single table with all redactions
+        skin.write_text_on(write, &all_redactions_md)?; // Write the entire table
     } else {
         // do nothing for now
         // Handle the case where there is no Redaction
--- icann-rdap-client/src/md/redacted.rs
+++ icann-rdap-client/src/md/redacted.rs
@@ -8,42 +8,133 @@ use icann_rdap_common::{
 
 use super::{string::StringUtil, table::MultiPartTable, MdParams, ToMd};
 
-impl ToMd for Redacted {
+// multi table version
+// impl ToMd for Redacted {
+//     fn to_md(&self, params: MdParams) -> String {
+//         // debug!("Redacted::to_md");
+//         let typeid = TypeId::of::<Redacted>();
+//         let mut md = String::new();
+//         md.push_str(&self.common.to_md(params.from_parent(typeid)));
+
+//         // header
+//         let header_text = "Redacted".to_string();
+
+//         md.push_str(&header_text.to_header(params.heading_level, params.options));
+
+//         // multipart data
+//         let mut table = MultiPartTable::new();
+
+//         table = table
+//             .header_ref(&"Fields")
+//             .and_data_ref(
+//                 &"name",
+//                 &self.name.description.as_ref().map(|s| s.clone()).or(self
+//                     .name
+//                     .type_field
+//                     .as_ref()
+//                     .map(|s| s.clone())),
+//             )
+//             .and_data_ref(&"prePath", &self.pre_path)
+//             .and_data_ref(&"postPath", &self.post_path)
+//             .and_data_ref(&"replacementPath", &self.replacement_path)
+//             .and_data_ref(&"pathLang", &self.path_lang)
+//             .and_data_ref(&"method", &self.method.as_ref().map(|m| m.to_string()))
+//             .and_data_ref(&"reason", &self.reason.as_ref().map(|m| m.to_string()));
+//         // checks
+//         let check_params = CheckParams::from_md(params, typeid);
+//         let mut checks = self.common.get_sub_checks(check_params);
+//         checks.push(self.get_checks(check_params));
+//         table = checks_to_table(checks, table, params);
+//         // render table
+//         md.push_str(&table.to_md(params));
+//         md.push('\n');
+//         md
+//     }
+// }
+
+// single table version but is missing the borders
+// impl ToMd for Redacted {
+//     fn to_md(&self, params: MdParams) -> String {
+//         let data = vec![
+//             self.name
+//                 .description
+//                 .clone()
+//                 .or(self.name.type_field.clone())
+//                 .unwrap_or_default(),
+//             self.pre_path.clone().unwrap_or_default(),
+//             self.post_path.clone().unwrap_or_default(),
+//             self.replacement_path.clone().unwrap_or_default(),
+//             self.path_lang.clone().unwrap_or_default(),
+//             self.method
+//                 .as_ref()
+//                 .map(|m| m.to_string())
+//                 .unwrap_or_default(),
+//             self.reason
+//                 .as_ref()
+//                 .map(|m| m.to_string())
+//                 .unwrap_or_default(),
+//         ];
+
+//         let fields = vec![
+//             "name",
+//             "prePath",
+//             "postPath",
+//             "replacementPath",
+//             "pathLang",
+//             "method",
+//             "reason",
+//         ];
+
+//         let mut md: String = String::from("## Redacted\n\n"); // Add title
+
+//         md += &fields
+//             .iter()
+//             .zip(data.iter())
+//             .map(|(field, value)| format!("| {} | {} |\n", field, value))
+//             .collect::<Vec<String>>()
+//             .join(""); // Collect as Vec<String> and then join
+
+//         md
+//     }
+// }
+impl ToMd for &[Redacted] {
     fn to_md(&self, params: MdParams) -> String {
-        // debug!("Redacted::to_md");
         let typeid = TypeId::of::<Redacted>();
         let mut md = String::new();
-        md.push_str(&self.common.to_md(params.from_parent(typeid)));
 
         // header
         let header_text = "Redacted".to_string();
-
         md.push_str(&header_text.to_header(params.heading_level, params.options));
 
         // multipart data
         let mut table = MultiPartTable::new();
+        table = table.header_ref(&"Fields");
 
-        table = table
-            .header_ref(&"Fields")
-            .and_data_ref(
-                &"name",
-                &self.name.description.as_ref().map(|s| s.clone()).or(self
-                    .name
-                    .type_field
-                    .as_ref()
-                    .map(|s| s.clone())),
-            )
-            .and_data_ref(&"prePath", &self.pre_path)
-            .and_data_ref(&"postPath", &self.post_path)
-            .and_data_ref(&"replacementPath", &self.replacement_path)
-            .and_data_ref(&"pathLang", &self.path_lang)
-            .and_data_ref(&"method", &self.method.as_ref().map(|m| m.to_string()))
-            .and_data_ref(&"reason", &self.reason.as_ref().map(|m| m.to_string()));
-        // checks
-        let check_params = CheckParams::from_md(params, typeid);
-        let mut checks = self.common.get_sub_checks(check_params);
-        checks.push(self.get_checks(check_params));
-        table = checks_to_table(checks, table, params);
+        for redacted in *self {
+            table = table
+                .and_data_ref(
+                    &"name",
+                    &redacted
+                        .name
+                        .description
+                        .as_ref()
+                        .map(|s| s.clone())
+                        .or(redacted.name.type_field.as_ref().map(|s| s.clone())),
+                )
+                .and_data_ref(&"prePath", &redacted.pre_path)
+                .and_data_ref(&"postPath", &redacted.post_path)
+                .and_data_ref(&"replacementPath", &redacted.replacement_path)
+                .and_data_ref(&"pathLang", &redacted.path_lang)
+                .and_data_ref(&"method", &redacted.method.as_ref().map(|m| m.to_string()))
+                .and_data_ref(&"reason", &redacted.reason.as_ref().map(|m| m.to_string()));
+
+            // checks
+            let check_params = CheckParams::from_md(params, typeid);
+            let mut checks = redacted.common.get_sub_checks(check_params);
+            checks.push(redacted.get_checks(check_params));
+            table = checks_to_table(checks, table, params);
+        }
+
         // render table
         md.push_str(&table.to_md(params));
         md.push('\n');
