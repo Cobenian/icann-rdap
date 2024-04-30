a5c75a5fed4dec8fa03bdb67ea7cf02d68c82354
Adam Guyot <adam@cobenian.com>
2024-04-30 16:39:28 -0400

    move over all the sanity checking and logic from json_path_match repo

--- icann-rdap-common/src/utils.rs
+++ icann-rdap-common/src/utils.rs
@@ -4,13 +4,12 @@ use jsonpath_rust::{JsonPathFinder, JsonPathInst};
 use regex::Regex;
 use response::RdapResponse;
 use serde_json::{json, Value};
-use std::collections::HashMap;
 use std::str::FromStr;
 
 use crate::response;
 
 // These are the different types of results that we can get from the JSON path checks
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum ResultType {
     Empty1, // (*) what we found in the value paths array was a string but has no value (yes, this is a little weird, but does exist) `Redaction by Empty Value`
     Empty2, // (*) what we found in the value paths array was a string but it is an empty string `Redaction by Empty Value`
@@ -24,11 +23,13 @@ pub enum ResultType {
     Removed5, // what finder.find_as_path() returned was not a Value::Array (have never found this, could possibly be an error)
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct RedactedObject {
     pub name: Value,
     pub pre_path: Option<String>,
     pub post_path: Option<String>,
+    pub final_path: Option<String>,
+    pub final_path_exists: bool,
     pub path_lang: Value,
     pub replacement_path: Option<String>,
     pub method: Value,
@@ -40,61 +41,274 @@ pub struct RedactedObject {
 pub fn replace_redacted_items(orignal_response: RdapResponse) -> RdapResponse {
     let rdap_json = serde_json::to_string(&orignal_response).unwrap();
     let mut v: Value = serde_json::from_str(&rdap_json).unwrap();
-    let mut respone = orignal_response; // Initialize with the original response
+    let mut response = orignal_response; // Initialize with the original response
 
     // if there are any redactions we need to do some modifications
     if let Some(redacted_array) = v["redacted"].as_array() {
-        let result = parse_redacted_array(redacted_array);
-        dbg!(&result);
+        let result = parse_redacted_array(&v, redacted_array);
+        // dbg!(&result);
 
-        // Get the paths that we need to redact, the pre and post paths but not the replacementValue ones
-        let pre_and_post_paths: Vec<&str> = result
-            .iter()
-            .filter(|item| item.method != Value::String("replacementValue".to_string()))
-            .filter_map(|item| {
-                item.pre_path
-                    .as_deref()
-                    .or_else(|| item.post_path.as_deref())
-            })
-            .collect();
+        for redacted_object in result {
+            println!("Processing redacted_object...");
+            if redacted_object.final_path_exists {
+                println!("final_path_exists is true");
+                if let Some(final_path) = redacted_object.final_path {
+                    println!("Found final_path: {}", final_path);
+                    dbg!(&final_path);
+                    match replace_with(v.clone(), &final_path, &mut |v| {
+                        println!("Replacing value...");
+                        if v.is_string() {
+                            match v.as_str() {
+                                Some("") => {
+                                    println!("Value is an empty string");
+                                    Some(json!("*REDACTED*"))
+                                }
+                                Some(s) => {
+                                    println!("Value is a string: {}", s);
+                                    Some(json!(format!("*{}*", s)))
+                                }
+                                _ => {
+                                    println!("Value is a non-string");
+                                    Some(json!("*REDACTED*"))
+                                }
+                            }
+                        }
+                        // the real question is what do we do with these types of values?
+                        else if v.is_null() {
+                            println!("Value is null");
+                            Some(json!(null))
+                        } else if v.is_boolean() {
+                            // what do we do?
+                            println!("Value is a boolean");
+                            Some(json!(false))
+                        } else if v.is_number() {
+                            // what do we do?
+                            println!("Value is a number");
+                            Some(json!(0))
+                        } else if v.is_array() {
+                            // what do we do?
+                            println!("Value is an array");
+                            Some(json!([]))
+                        } else if v.is_object() {
+                            // what do we do?
+                            println!("Value is an object");
+                            Some(json!({}))
+                        } else {
+                            // Handle non-string values here /// mon dieu! we cannot set this to a string!
+                            println!("Value is not a string");
+                            Some(json!("*NON-STRING VALUE*"))
+                        }
+                    }) {
+                        Ok(val) => {
+                            println!("Successfully replaced value");
+                            v = val; // No need to declare `v` as mutable again
+                        }
+                        Err(e) => {
+                            println!("Error replacing value: {}", e);
+                        }
+                    }
+                }
+            } else {
+                println!("final_path_exists is false");
+            }
+        }
+        // find all the replacementValues and replace them with the value in the replacementPath
 
-        // foreach of those, replace them with the *REDACTED* value
-        for path in pre_and_post_paths {
-            let json_path = path;
-            // dbg!(&json_path);
-            // remove the $ from json_path
-            // let json_path = json_path.trim_start_matches('$');
-            match replace_with(v.clone(), json_path, &mut |v| match v.as_str() {
-                Some("") => Some(json!("*REDACTED*")),
-                Some(s) => Some(json!(format!("*{}*", s))),
-                _ => Some(json!("*REDACTED*")),
-            }) {
-                Ok(val) => v = val,
-                Err(e) => {
-                    eprintln!("Error replacing value: {}", e);
+        response = serde_json::from_value(v).unwrap();
+    }
+
+    // Return the response
+    response
+}
+
+// everything else below this line is internal to the module
+
+fn parse_redacted_array(v: &Value, redacted_array: &Vec<Value>) -> Vec<RedactedObject> {
+    let mut result: Vec<RedactedObject> = Vec::new();
+
+    for item in redacted_array {
+        let item_map = item.as_object().unwrap();
+        let pre_path = item_map
+            .get("prePath")
+            .and_then(|v| v.as_str())
+            .map(|s| s.to_string());
+        let post_path = item_map
+            .get("postPath")
+            .and_then(|v| v.as_str())
+            .map(|s| s.to_string());
+        // we need set a final_path here, prefer pre over the post
+        let final_path = pre_path.clone().or(post_path.clone());
+        let mut redacted_object = RedactedObject {
+            name: Value::String(String::from("")), // Set to empty string initially
+            pre_path: pre_path,
+            post_path: post_path,
+            final_path: final_path.clone(),
+            final_path_exists: false, // Set to false initially
+            path_lang: item_map
+                .get("pathLang")
+                .unwrap_or(&Value::String(String::from("")))
+                .clone(),
+            replacement_path: item_map
+                .get("replacementPath")
+                .and_then(|v| v.as_str())
+                .map(|s| s.to_string()),
+            method: item_map
+                .get("method")
+                .unwrap_or(&Value::String(String::from("")))
+                .clone(),
+            reason: Value::String(String::from("")), // Set to empty string initially
+            result_type: None,                       // Set to None initially
+        };
+
+        // Check if the "name" field is an object
+        if let Some(Value::Object(name_map)) = item_map.get("name") {
+            // If the "name" field contains a "description" or "type" field, use it to replace the "name" field in the RedactedObject
+            if let Some(name_value) = name_map.get("description").or_else(|| name_map.get("type")) {
+                redacted_object.name = name_value.clone();
+            }
+        }
+
+        // Check if the "reason" field is an object
+        if let Some(Value::Object(reason_map)) = item_map.get("reason") {
+            // If the "reason" field contains a "description" or "type" field, use it to replace the "reason" field in the RedactedObject
+            if let Some(reason_value) = reason_map
+                .get("description")
+                .or_else(|| reason_map.get("type"))
+            {
+                redacted_object.reason = reason_value.clone();
+            }
+        }
+
+        // here is our sanity checking
+        if let Some(_final_path_str) = final_path {
+            redacted_object = set_result_type_from_json_path(v.clone(), redacted_object);
+            match redacted_object.result_type {
+                // if you are changing what is considered a "valid" path, you need to change this
+                Some(ResultType::Empty1)
+                | Some(ResultType::Empty2)
+                | Some(ResultType::Replaced1) => {
+                    redacted_object.final_path_exists = true;
+                }
+                _ => {
+                    redacted_object.final_path_exists = false;
                 }
             }
         }
 
-        // find all the replacementValues and replace them with the value in the replacementPath
-        let _replacement_value_paths: Vec<&str> = result
-            .iter()
-            .filter(|item| item.method == Value::String("replacementValue".to_string()))
-            .filter_map(|item| {
-                item.pre_path
-                    .as_deref()
-                    .or_else(|| item.post_path.as_deref())
-            })
-            .collect();
-
-        // Convert the modified Value back to RdapResponse
-        respone = serde_json::from_value(v).unwrap();
+        result.push(redacted_object);
     }
 
-    // Return the response
-    respone
+    result
 }
 
+pub fn set_result_type_from_json_path(u: Value, mut item: RedactedObject) -> RedactedObject {
+    if let Some(path) = item.final_path.as_deref() {
+        let path = path.trim_matches('"'); // Remove double quotes
+        match JsonPathInst::from_str(path) {
+            Ok(json_path) => {
+                let finder = JsonPathFinder::new(Box::new(u.clone()), Box::new(json_path));
+                let matches = finder.find_as_path();
+
+                if let Value::Array(paths) = matches {
+                    if paths.is_empty() {
+                        item.result_type = Some(ResultType::Removed1);
+                    } else {
+                        for path_value in paths {
+                            if let Value::String(found_path) = path_value {
+                                item.final_path = Some(found_path.clone()); // Assign found_path to final_path
+                                let no_value = Value::String("NO_VALUE".to_string());
+                                let re = Regex::new(r"\.\[|\]").unwrap();
+                                let json_pointer = found_path
+                                    .trim_start_matches('$')
+                                    .replace('.', "/")
+                                    .replace("['", "/")
+                                    .replace("']", "")
+                                    .replace('[', "/")
+                                    .replace(']', "")
+                                    .replace("//", "/");
+                                let json_pointer = re.replace_all(&json_pointer, "/").to_string();
+                                let value_at_path = u.pointer(&json_pointer).unwrap_or(&no_value);
+                                if value_at_path.is_string() {
+                                    let str_value = value_at_path.as_str().unwrap_or("");
+                                    if str_value == "NO_VALUE" {
+                                        item.result_type = Some(ResultType::Empty1);
+                                    } else if str_value.is_empty() {
+                                        item.result_type = Some(ResultType::Empty2);
+                                    } else {
+                                        item.result_type = Some(ResultType::Replaced1);
+                                    }
+                                } else if value_at_path.is_null() {
+                                    item.result_type = Some(ResultType::Removed2);
+                                } else if value_at_path.is_array() {
+                                    item.result_type = Some(ResultType::Replaced2);
+                                } else if value_at_path.is_object() {
+                                    item.result_type = Some(ResultType::Replaced3);
+                                } else {
+                                    item.result_type = Some(ResultType::Removed3);
+                                }
+                            } else {
+                                item.result_type = Some(ResultType::Removed4);
+                            }
+                        }
+                    }
+                } else {
+                    item.result_type = Some(ResultType::Removed5);
+                }
+            }
+            Err(e) => {
+                println!("Failed to parse JSON path '{}': {}", path, e);
+            }
+        }
+    }
+    item
+}
+
+pub fn check_valid_json_path(u: Value, path: &str) -> bool {
+    match JsonPathInst::from_str(path) {
+        Ok(json_path) => {
+            let finder = JsonPathFinder::new(Box::new(u.clone()), Box::new(json_path));
+            let matches = finder.find_as_path();
+
+            if let Value::Array(paths) = matches {
+                if !paths.is_empty() {
+                    for path_value in paths {
+                        if let Value::String(found_path) = path_value {
+                            let no_value = Value::String("NO_VALUE".to_string());
+                            let re = Regex::new(r"\.\[|\]").unwrap();
+                            let json_pointer = found_path
+                                .trim_start_matches('$')
+                                .replace('.', "/")
+                                .replace("['", "/")
+                                .replace("']", "")
+                                .replace('[', "/")
+                                .replace(']', "")
+                                .replace("//", "/");
+                            let json_pointer = re.replace_all(&json_pointer, "/").to_string();
+                            let value_at_path = u.pointer(&json_pointer).unwrap_or(&no_value);
+                            if value_at_path.is_string() {
+                                let str_value = value_at_path.as_str().unwrap_or("");
+                                if str_value == "NO_VALUE" {
+                                    // This is where Empty1 would be
+                                    return true;
+                                } else if str_value.is_empty() {
+                                    // This is where Empty2 would be
+                                    return true;
+                                } else {
+                                    // This is where Replaced1 would be
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            false
+        }
+        Err(_) => false,
+    }
+}
+
+// old stuff
 pub fn find_paths_to_redact(checks: &[(ResultType, String, String)]) -> Vec<String> {
     checks
         .iter()
@@ -206,73 +420,6 @@ pub fn get_redacted_paths_for_object(
             .collect(),
         _ => vec![],
     }
-}
-
-// pull the JSON paths from prePath and postPath
-// Section 4.2 you are not allowed to have both prePath and postPath in the same object
-// so right now, prePath is first choice.
-// pub fn get_pre_and_post_paths(paths: Vec<(String, Value, String)>) -> Vec<String> {
-//     paths
-//         .into_iter()
-//         .filter(|(key, _, _)| key == "prePath" || key == "postPath")
-//         .filter_map(|(_, value, _)| value.as_str().map(|s| s.to_string()))
-//         .collect()
-// }
-
-fn parse_redacted_array(redacted_array: &Vec<Value>) -> Vec<RedactedObject> {
-    let mut result: Vec<RedactedObject> = Vec::new();
-
-    for item in redacted_array {
-        let item_map = item.as_object().unwrap();
-        let mut redacted_object = RedactedObject {
-            name: Value::String(String::from("")), // Set to empty string initially
-            pre_path: item_map
-                .get("prePath")
-                .and_then(|v| v.as_str())
-                .map(|s| s.to_string()),
-            path_lang: item_map
-                .get("pathLang")
-                .unwrap_or(&Value::String(String::from("")))
-                .clone(),
-            replacement_path: item_map
-                .get("replacementPath")
-                .and_then(|v| v.as_str())
-                .map(|s| s.to_string()),
-            post_path: item_map
-                .get("postPath")
-                .and_then(|v| v.as_str())
-                .map(|s| s.to_string()),
-            method: item_map
-                .get("method")
-                .unwrap_or(&Value::String(String::from("")))
-                .clone(),
-            reason: Value::String(String::from("")), // Set to empty string initially
-            result_type: None,                       // Set to None initially
-        };
-
-        // Check if the "name" field is an object
-        if let Some(Value::Object(name_map)) = item_map.get("name") {
-            // If the "name" field contains a "description" or "type" field, use it to replace the "name" field in the RedactedObject
-            if let Some(name_value) = name_map.get("description").or_else(|| name_map.get("type")) {
-                redacted_object.name = name_value.clone();
-            }
-        }
-
-        // Check if the "reason" field is an object
-        if let Some(Value::Object(reason_map)) = item_map.get("reason") {
-            // If the "reason" field contains a "description" or "type" field, use it to replace the "reason" field in the RedactedObject
-            if let Some(reason_value) = reason_map
-                .get("description")
-                .or_else(|| reason_map.get("type"))
-            {
-                redacted_object.reason = reason_value.clone();
-            }
-        }
-
-        result.push(redacted_object);
-    }
-
-    result
 }
 
 // Adds a field to the JSON object
